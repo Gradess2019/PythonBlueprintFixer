06270c233d4b9317a76541e73ee366beee6567fd
Gradess <trofimovjava@gmail.com>
2023-05-16 20:04:13 +0200

    Setup plugin

---
+++ Source/PythonBlueprintFixer/Private/PythonBlueprintFixer.cpp
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PythonBlueprintFixer.h"
+
+#define LOCTEXT_NAMESPACE "FPythonBlueprintFixerModule"
+
+void FPythonBlueprintFixerModule::StartupModule()
+{
+}
+
+void FPythonBlueprintFixerModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FPythonBlueprintFixerModule, PythonBlueprintFixer)
